7dcd3ad46f61142cd60051e2e6ec50ebf76d9dca
Lyubov Si <gse182@yandex.ru>
2022-10-15 11:05:45 +0300

    Создан метод, который в исходном массиве ищет элементы с длиной <=3  и добавляет в новый массив, который создан внутри метода

--- task/Program.cs
+++ task/Program.cs
@@ -14,7 +14,7 @@ string[] GetArray(int size)
     //заполнение массива
     for (int i = 0; i < array.Length; i++)
     {
-        array[i]=Console.ReadLine();
+        array[i] = Convert.ToString(Console.Read());
     }
 
     return array;
@@ -23,7 +23,7 @@ string[] GetArray(int size)
 ///вывод массива на экран
 void PrintArray(string[] arr)
 {
-    Console.WriteLine("Start array: ");
+    
     for (int i = 0; i < arr.Length; i++)
     {
         Console.Write($" {arr[i]}    ");
@@ -33,19 +33,38 @@ void PrintArray(string[] arr)
 ///метод считает кол-во элементов с длиной <=3
 int CountWord(string[] arr)
 {
-    int count=0;
+    int count = 0;
     for (int i = 0; i < arr.Length; i++)
     {
-        if (arr[i].Length<=3) count++;
+        if (arr[i].Length <= 3) count++;
     }
 
     return count;
 }
 
-string[] array1 = GetArray(size);
-PrintArray(array1);
+string[] SearchElement(string[] arr, int size)
+{
+    string[] array2 = new string[size];
+    int count = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i].Length <= 3)
+        {
+            array2[count]=arr[i];
+            count+=1;
+        }
+    }
+    return array2;
+}
 
-string[] array2 = new string[CountWord(array1)];
+string[] array1 = GetArray(size);
+Console.WriteLine("Start array: ");
+PrintArray(array1);
+Console.WriteLine();
+Console.WriteLine("Result array: ");
+PrintArray(SearchElement(array1,CountWord(array1)));
+
+
 
 
 
